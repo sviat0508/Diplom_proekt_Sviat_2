bafd05fe76eefb87c1b5d6e0172bb14fcb2a6ee1
Sviat <svaitoslavtt@email.com>
2024-09-14 20:06:59 +0300

    fifs commit

--- script.js
+++ script.js
@@ -37,3 +37,36 @@ window.addEventListener('scroll', () => {
         });
     }
 });
+
+
+const modals = [
+  { modal: document.getElementById("modal1"), btn: document.getElementById("modal_info1"), close: document.getElementById("close1") },
+  { modal: document.getElementById("modal2"), btn: document.getElementById("modal_info2"), close: document.getElementById("close2") },
+  { modal: document.getElementById("modal3"), btn: document.getElementById("modal_info3"), close: document.getElementById("close3") },
+  { modal: document.getElementById("modal4"), btn: document.getElementById("modal_info4"), close: document.getElementById("close4") }
+];
+
+modals.forEach((item) => {
+  item.btn.onclick = function() {
+      item.modal.style.display = "flex"; 
+      setTimeout(() => {
+          item.modal.classList.add("show");
+      }, 10); 
+  }
+
+  item.close.onclick = function() {
+      item.modal.classList.remove("show");
+      setTimeout(() => {
+          item.modal.style.display = "none";
+      }, 300); 
+  }
+
+  window.onclick = function(event) {
+      if (event.target === item.modal) {
+          item.modal.classList.remove("show");
+          setTimeout(() => {
+              item.modal.style.display = "none";
+          }, 300);
+      }
+  }
+});
